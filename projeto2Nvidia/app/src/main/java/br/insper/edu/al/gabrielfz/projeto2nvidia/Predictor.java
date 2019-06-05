477ae2a2916bbbf4e2bbfdda617f35cd394efaf8
Gabriel <gabrielzezze@me.com>
2019-06-05 16:37:50 -0300

    AppDone

--- projeto2Nvidia/app/src/main/java/br/insper/edu/al/gabrielfz/projeto2nvidia/Predictor.java
+++ projeto2Nvidia/app/src/main/java/br/insper/edu/al/gabrielfz/projeto2nvidia/Predictor.java
@@ -8,7 +8,7 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-public class Predictor extends AsyncTask<String, Integer, Long> {
+public class Predictor{
     private final int scientistsNumber;
     private final String dataType;
     private final int Projects;
@@ -111,36 +111,6 @@ public class Predictor extends AsyncTask<String, Integer, Long> {
         else{
             return this.predictCloud();
         }
-    }
-    @Override
-    public Long doInBackground(String... params){
-        try {
-            System.out.println("RODEI");
-            URL url = new URL("https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarDia(dataCotacao=@dataCotacao)?@dataCotacao='06-05-2019'&$top=1&$format=text/csv");
-            HttpURLConnection con = (HttpURLConnection) url.openConnection();
-            con.setRequestMethod("GET");
-            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-            String inputLine;
-            StringBuffer content = new StringBuffer();
-            while ((inputLine = in.readLine()) != null) {
-                content.append(inputLine);
-            }
-            System.out.println(content);
-            in.close();
-        } catch(IOException e){
-            System.out.println("ERROR");
-        }
-        long someLong = (long) 0.1;
-        return someLong;
-    }
-
-    @Override
-    public void onPostExecute(Long result) {
-        isDone = true;
-    }
-
-    public boolean isDone() {
-        return isDone;
     }
 
 }
